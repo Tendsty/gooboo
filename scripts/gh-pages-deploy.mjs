b662c8eed872250c9e83c8f81984e25518b89884
Tendsty <137620092+Tendsty@users.noreply.github.com>
2023-09-23 16:40:56 +0200

    update script

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -1,15 +1,12 @@
 /* eslint-disable no-console */
-import execa from "execa";
-import fs from "fs";
+import { execa } from "execa";
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
-    // await execa("yarn", ["build"]);
-    // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    const folderName = "dist";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
