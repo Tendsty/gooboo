747f5dd4beb224a7a1d83949aef342003983e563
Tendsty <137620092+Tendsty@users.noreply.github.com>
2023-09-23 16:22:51 +0200

    update imports

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-const execa = require("execa");
-const fs = require("fs");
+import execa from "execa";
+import fs from "fs";
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
