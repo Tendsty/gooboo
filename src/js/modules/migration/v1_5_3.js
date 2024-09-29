837ce915b3faefadbe535160058f3de2c6ed8e1a
Anti27 <20539129+Anti27@users.noreply.github.com>
2024-09-29 14:46:22 +0200

    Update v1_5_3.js

--- src/js/modules/migration/v1_5_3.js
+++ src/js/modules/migration/v1_5_3.js
@@ -1,10 +1,35 @@
 export default function(save) {
   
     // Fix offerings
-    if (save.village && save.village.offering) {
-        for (const [key, elem] of Object.entries(save.village.offering)) {
-            save.village.offering[key] = [elem.offeringBought, elem.upgradeBought];
+    if (save.village && save.village.offering && Object.is(save.currency.village_offering , NaN)) {
+        let currentAmount = save.stat.village_offeringMax[1];
+        ['plantFiber', 'wood', 'stone'].forEach(elem => {
+            if (save.village.offering[elem] !== undefined) {
+                currentAmount -= save.village.offering[elem].upgradeBought;
+            }
+        });
+        ['coin', 'metal', 'water'].forEach(elem => {
+            if (save.village.offering[elem] !== undefined) {
+                currentAmount -= save.village.offering[elem].upgradeBought * 3;
+            }
+        });
+        ['glass', 'hardwood', 'gem'].forEach(elem => {
+            if (save.village.offering[elem] !== undefined) {
+                currentAmount -= save.village.offering[elem].upgradeBought * 8;
+            }
+        });
+        save.currency.village_offering = currentAmount;
+
+        if (save.village.offering.knowledge !== undefined) {
+            save.village.offering.knowledge[1] = 0;
         }
+        if (save.village.offering.science !== undefined) {
+            save.village.offering.science[1] = 0;
+        }
+        if (save.village.offering.joy !== undefined) {
+            save.village.offering.joy[1] = 0;
+        }
+        
     }
 
     return save;
