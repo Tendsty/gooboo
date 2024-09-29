731398279f2ed762312a865c97516f836e06e713
Anti27 <20539129+Anti27@users.noreply.github.com>
2024-09-29 17:11:44 +0200

    Update v1_5_3.js

--- src/js/modules/migration/v1_5_3.js
+++ src/js/modules/migration/v1_5_3.js
@@ -45,9 +45,9 @@ export default function(save) {
     //Fix Merchant items
     if (save.event && save.event.shop_merchant.length > 0){
          save.event.shop_merchant.forEach(elem => {
-            if (save.event.shop_merchant[elem].data !== null && save.event.shop_merchant[elem].data.effect.length > 0) {
+            if (elem.data !== null && elem.data.effect.length > 0) {
                 elem.data.effect.forEach(effect => {
-                    save.event.shop_merchant[elem].data.effect[effect].replace("currencyFarmFruitGain","currencyFarmBerryGain")
+                    effect.replace("currencyFarmFruitGain","currencyFarmBerryGain")
                 });
             }
         });
@@ -56,7 +56,7 @@ export default function(save) {
     //some might have bought it
     if (save.treasure && save.treasure.newItem && save.treasure.newItem.effect.length > 0 ){
         save.treasure.newItem.effect.forEach(effect => {
-            save.treasure.newItem.effect[effect].replace("currencyFarmFruitGain","currencyFarmBerryGain")
+            effect.replace("currencyFarmFruitGain","currencyFarmBerryGain")
         });
     }    
 
