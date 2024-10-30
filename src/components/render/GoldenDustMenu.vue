bdde56bbf0fe985aef68eeafb449ccbb34461b5f
baicy <baicy0118@gmail.com>
2024-10-30 16:39:02 +0800

    cheat: 不消耗金尘

--- src/components/render/GoldenDustMenu.vue
+++ src/components/render/GoldenDustMenu.vue
@@ -54,7 +54,8 @@ export default {
   computed: {
     ...mapGetters({
       mainFeatures: 'system/mainFeatures',
-      isOnMainFeature: 'system/isOnMainFeature'
+      isOnMainFeature: 'system/isOnMainFeature',
+      isMe: 'system/isMe'
     }),
     isValidTime() {
       return !isNaN(this.minutes) && this.minutes > 0;
@@ -83,7 +84,9 @@ export default {
       if (this.canAfford) {
         const module = {mining, village, horde, farm, gallery}[this.$store.state.system.screen];
         module.tick(Math.round(this.minutes * 60 / module.tickspeed));
-        this.$store.dispatch('currency/spend', {feature: 'school', name: 'goldenDust', amount: this.dustCost});
+        if(!this.isMe) {
+          this.$store.dispatch('currency/spend', {feature: 'school', name: 'goldenDust', amount: this.dustCost});
+        }
       }
     },
     convertPass() {
